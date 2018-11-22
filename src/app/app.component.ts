97f321d7a39b4ae0825968f078bde781a4c3e8cb
Sergey Pashnin <s.pashnin@encry.ru>
2018-11-22 13:36:43 +0300

    add multi animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,16 +25,52 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
             transition('start <=> end', animate('800ms ease')),
             transition('start => active', animate(700)),
             transition('active => end', animate(700)),
+        ]),
+        // делаем более сложную анимацию
+        trigger('multi', [
+            state('start', style({
+                width: '150px',
+                height: '150px',
+                border: '5px solid black'
+            })),
+            state('end', style({
+                width: '170px',
+                height: '170px',
+                background: 'blue'
+            })),
+            // задаем анимацию в transition
+            transition('start <=> end', [
+                style({
+                    background: 'red'
+                }),
+                animate(1700, style({
+                    background: 'yellow'
+                })),
+                animate(1000, style({
+                    width: '200px',
+                    height: '200px',
+                })),
+                animate(1000)
+            ])
         ])
     ]
 })
 export class AppComponent {
     clickedDivState = 'start';
+    multiState = 'start';
 
     changeDivState() {
         this.clickedDivState = 'end';
         setTimeout(() => {
             this.clickedDivState = 'start';
         }, 3000)
+    }
+
+    changeMultiState(){
+        if (this.multiState === 'start'){
+            this.multiState = 'end';
+        } else {
+            this.multiState = 'start';
+        }
     }
 }
