0ab10190385c3e260ec2e8e074a6a72e077448d0
Sergey Pashnin <s.pashnin@encry.ru>
2018-11-22 12:49:49 +0300

    add animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    animations: [
+        // при создании анимации необходимо придерживаться определенной иерархии
+        // для начала нужно дать название анимации
+        // это делается с помощью метода trigger()
+        // первым параметром задается название анимации
+        // вторым - массив, в котором задаюся определенные состояния (до / после анимации)
+
+        // state() - определяет состояние анимации
+        // 1й парам - название состояния
+        // 2й парам - стили состояния
+
+        // по умолчанию ng принимает значения в %
+
+        trigger('clickedDiv', [
+            state('start', style({
+                backgroundColor: 'blue',
+                width: '150px',
+                height: '150px',
+            })),
+            state('end', style({
+                backgroundColor: 'red',
+                width: '150px',
+                height: '150px',
+                borderRadius: '50%'
+            })),
+            // отвечает за анимацию
+            // 1й - описывает изменение состояний
+            // 2й - animate() - принимает различные параметры
+            // animate('800ms .5s ease-out') - ('скорость задержка тип-анимации')
+            transition('start => end', animate(1500)),
+            transition('end => start', animate('800ms .5s ease-out'))
+        ])
+    ]
 })
 export class AppComponent {
+    // переменная принимает значение определенного стейта
+    clickedDivState = 'start';
+
+    changeDivState() {
+        this.clickedDivState = 'end';
+        setTimeout(() => {
+            this.clickedDivState = 'start';
+        }, 3000)
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        // подключаем модуль для работы с анимацией
+        // важно, что бы был подключен пакет "@angular/animations"
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
