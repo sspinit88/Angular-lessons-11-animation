f3ab95e0638a7ad711c6f4ff80f4310b9585f1a3
Sergey Pashnin <s.pashnin@encry.ru>
2018-11-22 13:10:42 +0300

    add ыефstates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,6 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
     selector: 'app-root',
     templateUrl: './app.component.html',
     animations: [
-        // при создании анимации необходимо придерживаться определенной иерархии
-        // для начала нужно дать название анимации
-        // это делается с помощью метода trigger()
-        // первым параметром задается название анимации
-        // вторым - массив, в котором задаюся определенные состояния (до / после анимации)
-
-        // state() - определяет состояние анимации
-        // 1й парам - название состояния
-        // 2й парам - стили состояния
-
-        // по умолчанию ng принимает значения в %
-
         trigger('clickedDiv', [
             state('start', style({
                 backgroundColor: 'blue',
@@ -25,21 +13,22 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
             })),
             state('end', style({
                 backgroundColor: 'red',
-                width: '150px',
-                height: '150px',
-                borderRadius: '50%'
+                width: '250px',
+                height: '250px',
             })),
-            // отвечает за анимацию
-            // 1й - описывает изменение состояний
-            // 2й - animate() - принимает различные параметры
-            // animate('800ms .5s ease-out') - ('скорость задержка тип-анимации')
-            transition('start => end', animate(1500)),
-            transition('end => start', animate('800ms .5s ease-out'))
+
+            state('active', style({
+                width: '160px',
+                height: '160px',
+                backgroundColor: 'orange',
+            })),
+            transition('start <=> end', animate('800ms ease')),
+            transition('start => active', animate(700)),
+            transition('active => end', animate(700)),
         ])
     ]
 })
 export class AppComponent {
-    // переменная принимает значение определенного стейта
     clickedDivState = 'start';
 
     changeDivState() {
